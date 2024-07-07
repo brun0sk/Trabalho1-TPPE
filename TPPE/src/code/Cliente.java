88011e28f043a9cfe28c029c9910105baf0d3cdc
Thiago Borges <thiagocerq21@gmail.com>
2024-07-07 04:10:14 -0300

    Update Cliente.java

--- TPPE/src/code/Cliente.java
+++ TPPE/src/code/Cliente.java
@@ -3,25 +3,23 @@ package code;
 public class Cliente {
     private String nome;
     private String cpf;
+    private String endereco;
 
-    public Cliente(String nome, String cpf) {
+    public Cliente(String nome, String cpf, String endereco) {
         this.nome = nome;
         this.cpf = cpf;
+        this.endereco = endereco;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public String getEndereco() {
+        return endereco;
     }
 }
