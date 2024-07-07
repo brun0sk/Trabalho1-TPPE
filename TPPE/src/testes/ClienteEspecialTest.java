4e7a1e2acfd65f076fb2c72d687917a865ca3818
bruno <brunokishibe@hotmail.com>
2024-07-07 16:15:16 -0300

    Adiciona teste cliente especial.

---
+++ TPPE/src/testes/ClienteEspecialTest.java
@@ -0,0 +1,47 @@
+package testes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import code.ClientEspecial;
+
+public class ClienteEspecialTest {
+	private ClientEspecial clienteEspecial;
+
+    @Before
+    public void setUp() {
+        clienteEspecial= new ClientEspecial("Mario", "50345678901", "Rua A, 100");
+    }
+
+    @SuppressWarnings("deprecation")
+	@Test
+    public void testeDescontoCompraCartaoLoja() {
+        double valorCompra = 200.0;
+        String numeroCartao = "4296131111111111";
+        double desconto = clienteEspecial.calcularDesconto(valorCompra, numeroCartao);
+        double valorEsperado = valorCompra * 0.1; 
+        assertEquals(valorEsperado, desconto, 0.01);
+    }
+    
+    @Test
+    public void testeDescontoCompraSemCartaoLoja() {
+        double valorCompra = 200.0;
+        String numeroCartao = "4776131111111111";
+        double desconto = clienteEspecial.calcularDesconto(valorCompra, numeroCartao);
+        double valorEsperado = valorCompra; 
+        assertEquals(valorEsperado, desconto, 0.01);
+    }
+    
+    @Test
+    public void testeDescontoFrete() {
+    	double valorFrete = 15.0;
+        String numeroCartao = "4296131111111111";
+        double descontoFrete = clienteEspecial.calcularDescontoFrete(valorFrete, numeroCartao);
+        double valorEsperado = valorFrete * 0.3; 
+        assertEquals(valorEsperado, descontoFrete, 0.01);
+    }
+    
+}
